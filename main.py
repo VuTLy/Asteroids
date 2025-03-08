a684c051e0ad7939b7437f4dbd988159872976c9
VuTLy <vulyccschool@gmail.com>
2025-03-07 19:46:49 -0800

    2: drawing player white triangle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 
@@ -10,15 +11,33 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #initalizing game screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    #building FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #initiate player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+
+    #game loop
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit frame rate to 60fps
+        dt = clock.tick(60) / 1000
+        
+
 
 if __name__ == "__main__":
     main()
